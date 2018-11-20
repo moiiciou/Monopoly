0759521af72193d5d19d6bb84d557b19645f7ab6
Charles Still <charles.still@yahoo.fr>
2018-11-20 09:50:47 +0100

    Ajout Classes Charles

---
+++ Monopoly/CaseManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Controller
+{
+    static class CaseManager
+    {
+        public static int GetPrisonCasePosition()
+        {
+            return 0;
+        }
+    }
+}
