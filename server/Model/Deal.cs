bdde9a9ba750ba020384068b64193f228bf37114
Unknown <th.philipps@laposte.net>
2019-03-30 23:05:28 +0100

    add Deal Model

---
+++ server/Model/Deal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Model
+{
+    public class Deal
+    {
+        private int nextId = 0;
+        public int Id { get; }
+        public PlayerInfo Asker { get; set; }
+        public PlayerInfo Receiver { get; set; }
+
+        public bool Accepted { get; set; }
+
+        public Object ObjectTransfered { get; set; }
+
+        public Deal(PlayerInfo asker, PlayerInfo receiver, Object objectTransfered)
+        {
+            Id = nextId;
+            nextId++;
+            Asker = asker;
+            Receiver = receiver;
+            Accepted = false;
+            ObjectTransfered = objectTransfered;
+        }
+
+        public bool Process()
+        {
+            // si object est une propriété ... faire le traitement
+
+            // si object est de type carte libéré de prison, faire le traitement
+
+            //return true si tout c'est bien passé
+            return true;
+        }
+
+    }
+}
